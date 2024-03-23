94fb24342357c864412c13ae8c20066ab8f686aa
David Nzube <davidnzube2007@outlook.com>
2024-03-23 06:55:15 +0100

    2nd commit

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from website import initialize_app
 app = initialize_app()
 
 if __name__ == '__main__':
-  app.run(debug="True", host="0.0.0.0", port=9009)
+  # app.run(debug="True", host="0.0.0.0", port=9009)
+  app.run()
 
     
